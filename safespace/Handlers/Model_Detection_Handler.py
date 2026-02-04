d309a1c4ae35c23d38d0d670dd1bf59afaad4c07
ZiadMD <ziadmohamedgamal25@gmail.com>
2026-02-04 20:11:53 +0200

    Removed Roboflow Model Inference

--- safespace/Handlers/Model_Detection_Handler.py
+++ safespace/Handlers/Model_Detection_Handler.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 from inference.core.models.base import Model
 import supervision as sv
 from cv2.typing import MatLike
+import torch
 
 class ModelDetectionHandler:
 
@@ -17,22 +18,7 @@ class ModelDetectionHandler:
         Returns:
             List of detection results.
         """
+
         results = model.predict(frame, conf=confidence, verbose=False)
         detections = results.xyxy[0].cpu().numpy().tolist()  # Convert to list of detections
-        return detections
-    
-    def roboflow_detect(self, model: Model, confidence: float, frame: MatLike) -> list:
-        """
-        Perform object detection on a given frame using the Roboflow model.
-
-        Args:
-            model (Model): The loaded Roboflow model.
-            confidence (float): Confidence threshold for detections.
-            frame: The input frame/image for detection.
-
-        Returns:
-            List of detection results.
-        """
-        predictions = model.infer(frame, conf=confidence)[0]
-        detections = sv.Detections.from_inference(predictions)
         return detections
